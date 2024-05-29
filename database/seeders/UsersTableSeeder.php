349682e306cfe4c79b8bfd083f8a47cb794eec5d
zoshhhh <129791585+Zoshhhh@users.noreply.github.com>
2024-05-29 14:19:33 +0200

    Update UsersTableSeeder.php

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UsersTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds..
      */
     public function run()
     {
